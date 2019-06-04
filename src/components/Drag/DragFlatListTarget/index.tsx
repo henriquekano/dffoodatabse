0a1739591a4064cc68e5a8e4ed6d98e4ade5ed7e
Henrique Kano <henriquekano@hotmail.com>
2019-06-04 02:06:39 -0300

    drag-flat-list-target: version 2
     - Avoids unnecessary flat list cell rerender

--- src/components/Drag/DragFlatListTarget/index.tsx
+++ src/components/Drag/DragFlatListTarget/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { PureComponent } from 'react'
+import { PureComponent, Component } from 'react'
 import {
   FlatList,
   FlatListProps,
@@ -14,6 +14,89 @@ import {
 import { Subject } from 'rxjs'
 import { moveChannel, dropChannel, Message } from '../contex'
 import { DragTarget } from '../../index'
+
+interface RenderItemProps<T> {
+  item: T,
+  index: number,
+  separators: any,
+  hoveringIndex?: number,
+  droppedIndex?: number,
+  onLayout: any,
+  nameExtractor: (item: T, index: number) => string,
+  moveChannel: any,
+  dropChannel: any,
+  dragTargetRenderItem: (something: any) => any
+}
+
+class RenderItem<T> extends Component<RenderItemProps<T>> {
+  constructor (props: RenderItemProps<T>) {
+    super(props)
+    const { index, hoveringIndex, droppedIndex } = props
+    this.state = {
+      droppedHere: hoveringIndex === index,
+      hoveringHere: droppedIndex === index,
+    }
+  }
+
+  shouldComponentUpdate = (nextProps: RenderItem<T>) => {
+    const { index } = this.props
+    const { hoveringHere, droppedHere } = this.state
+    const { hoveringIndex: nextHovering, droppedIndex: nextDrop } = nextProps
+
+    const isHoveringHere = nextHovering === index
+    const isDroppedHere = nextDrop === index
+
+    const changedHoverState = hoveringHere !== isHoveringHere
+    const changedDropState = droppedHere !== isDroppedHere
+
+    return changedDropState || changedHoverState
+  }
+
+  componentWillReceiveProps = (nextProps: RenderItem<T>) => {
+    const { index } = this.props
+    const { hoveringIndex: nextHovering, droppedIndex: nextDrop } = nextProps
+
+    const nowItsHoveringHere = nextHovering === index
+    const nowItHasDroppedHere = nextDrop === index
+    this.setState({
+      droppedHere: nowItHasDroppedHere,
+      hoveringHere: nowItsHoveringHere,
+    })
+  }
+
+  render = () => {
+    const {
+      dragTargetRenderItem,
+      item,
+      index,
+      separators,
+      hoveringIndex,
+      droppedIndex,
+      nameExtractor,
+      moveChannel,
+      dropChannel,
+      onLayout,
+    } = this.props
+    return (
+      <DragTarget
+        onLayout={onLayout}
+        name={nameExtractor(item, index)}
+        moveChannel={moveChannel}
+        dropChannel={dropChannel}
+      >
+        {
+          dragTargetRenderItem({
+            item,
+            index,
+            separators,
+            draggableHovering: hoveringIndex === index,
+            draggableDropped: droppedIndex === index,
+          })
+        }
+      </DragTarget>
+    )
+  }
+}
 
 // Copied the following two from react's definitions
 export interface DraggableRenderItemInfo<ItemT> extends ListRenderItemInfo<ItemT> {
@@ -115,24 +198,74 @@ class DragFlatListTarget<T> extends PureComponent<DragFlatListTargetProps<T>> {
       return null
     }
 
-    let verticalSum = -this.scrollOffset.y
-    let index = 0
-    for (; index < this.itemsLayouts.length && verticalSum < y; index ++) {
-      verticalSum += this.itemsLayouts[index].height
-    }
+    try {
+      let verticalSum = -this.scrollOffset.y
+      let index = 0
+      for (; index < this.itemsLayouts.length && verticalSum < y; index ++) {
+        const currentItem = this.itemsLayouts[index]
+        verticalSum += currentItem.height
+      }
 
-    // The last step sums the index and the summed height is >= y
-    // i.e., it's pointing to the NEXT element
-    // the '-1' fixes that. Maybe there's a more elegant way to do it...
-    // Same logic with the verticalSum - this.itemsLayouts[index].height
-    return {
-      element: {
-        ...this.itemsLayouts[index - 1],
-        x: 0,
-        y: verticalSum - this.itemsLayouts[index].height,
-      },
-      index: index - 1,
+      // The last step sums the index and the summed height is >= y
+      // i.e., it's pointing to the NEXT element
+      // the '-1' fixes that. Maybe there's a more elegant way to do it...
+      // Same logic with the verticalSum - this.itemsLayouts[index].height
+      if (index < this.itemsLayouts.length - 1) {
+        return {
+          element: {
+            ...this.itemsLayouts[index - 1],
+            x: 0,
+            y: verticalSum - this.itemsLayouts[index].height,
+          },
+          index: index - 1,
+        }
+      }
+      // Edge case: it's the last item
+      return {
+        element: {
+          ...this.itemsLayouts[index - 1],
+          x: 0,
+          y: verticalSum - this.itemsLayouts[index - 1].height,
+        },
+        index: index - 1,
+      }
+    } catch (err) {
+      // Pretty sure that if the 'currentItem'
+      // is in process of changing, given the
+      // asynchronous trait of the rxjs subject usage
+      // of pan events, when calculating this sum,
+      // a exception may occur - easily reproduceable
+      // by dragging the draggable rapidly over the
+      // list items
+      return null
     }
+  }
+
+  renderItem = ({ item, index, separators }) => {
+    const {
+      hoveringIndex,
+      droppedIndex,
+    } = this.state
+    const {
+      renderItem: dragTargetRenderItem,
+      keyExtractor,
+      nameExtractor,
+    } = this.props
+    return (
+      <RenderItem
+        key={keyExtractor(item, index)}
+        onLayout={(e) => this.handleItemLayout(e, index)}
+        dragTargetRenderItem={dragTargetRenderItem}
+        dropChannel={this.internalDropChannel}
+        moveChannel={this.internalMoveChannel}
+        droppedIndex={droppedIndex}
+        hoveringIndex={hoveringIndex}
+        index={index}
+        item={item}
+        nameExtractor={nameExtractor}
+        separators={separators}
+      />
+    )
   }
 
   render = () => {
@@ -143,43 +276,13 @@ class DragFlatListTarget<T> extends PureComponent<DragFlatListTargetProps<T>> {
       data,
       ...rest
     } = this.props
-    const {
-      hoveringIndex,
-      droppedIndex,
-    } = this.state
-    console.log('rerendering!')
     return (
       <FlatList
         {...rest}
         extraData={this.state}
         data={data}
         onScroll={this.handleOffsetChange}
-        renderItem={({ item, index, separators }) => {
-          console.log('RERENDERIZING ' + index)
-          return (
-            <DragTarget
-              ref={ref => {
-                if (ref) {
-                  this.childrenRefs[index] = ref
-                }
-              }}
-              name={nameExtractor(item, index)}
-              moveChannel={this.internalDropChannel}
-              dropChannel={this.internalDropChannel}
-              onLayout={(e) => this.handleItemLayout(e, index)}
-            >
-              {
-                dragTargetRenderItem({
-                  item,
-                  index,
-                  separators,
-                  draggableHovering: hoveringIndex === index,
-                  draggableDropped: droppedIndex === index,
-                })
-              }
-            </DragTarget>
-          )
-        }}
+        renderItem={this.renderItem}
         onLayout={this.handleInitialLayout}
       />
     )
